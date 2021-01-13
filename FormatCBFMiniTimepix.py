25477afcab7d61823e5011d7c6ea71624c6e32e2
David Waterman <dgwaterman@gmail.com>
2021-01-13 16:58:34 +0000

    Add FormatCBFMiniTimepix516
    But there is a lot of code duplication still, which should be tidied up

--- FormatCBFMiniTimepix.py
+++ FormatCBFMiniTimepix.py
@@ -84,20 +84,14 @@ class FormatCBFMiniTimepix(FormatCBFMini):
     def _scan(self):
         """Dummy scan for this image"""
 
-        fname = os.path.split(self._image_file)[-1]
-        index = int(fname.split("_")[-1].split(".")[0])
-        return ScanFactory.make_scan((index, index), 0.0, (0, 1), {index: 0})
-
-    def _scan(self):
-        """Dummy scan for this image"""
-
-        format = self._scan_factory.format("CBF")
-
         exposure_time = float(self._cif_header_dictionary["Exposure_period"].split()[0])
 
         fname = os.path.split(self._image_file)[-1]
         index = int(fname.split("_")[-1].split(".")[0])
 
+        # Work around
+        # self._cif_header_dictionary["Start_angle"]
+        # self._cif_header_dictionary["Angle_increment"]
         return ScanFactory.make_scan(
             image_range=(index, index),
             exposure_times=exposure_time,
@@ -265,17 +259,125 @@ class FormatCBFMiniTimepix512(FormatCBFMiniTimepix):
         return d
 
 
+class FormatCBFMiniTimepix516(FormatCBFMiniTimepix):
+    @staticmethod
+    def understand(image_file):
+        """Check to see if this looks like a Timepix mini CBF format image,
+        i.e. we can make sense of it."""
+        mime_header = ""
+        in_binary_format_section = False
+        for record in FormatCBFMini.open_file(image_file, "rb"):
+            record = record.decode()
+            if "--CIF-BINARY-FORMAT-SECTION--" in record:
+                in_binary_format_section = True
+            elif in_binary_format_section and record.startswith("X"):
+                mime_header += record
+            if in_binary_format_section and len(record.strip()) == 0:
+                # http://sourceforge.net/apps/trac/cbflib/wiki/ARRAY_DATA%20Category
+                #    In an imgCIF file, the encoded binary data begins after
+                #    the empty line terminating the header.
+                break
+
+        # Look for 516 pixels
+        for record in mime_header.split("\n"):
+            if (
+                "-Binary-Size-Fastest-Dimension:" in record
+                and int(record.split()[1]) == 516
+            ):
+                return True
+
+        return False
+
+    def _detector(self):
+        """Dummy detector"""
+        from scitbx import matrix
+
+        # 55 mu pixels
+        pixel_size = 0.055, 0.055
+        trusted_range = (-1, 65535)
+        material = "Si"
+        thickness = 0.3  # assume 300 mu thick. This is actually in the header too
+        # so could take it from there
+
+        # Initialise detector frame - dummy origin to place detector at the header
+        # distance along the canonical beam direction
+        distance = (
+            float(self._cif_header_dictionary["Detector_distance"].split()[0]) * 1000
+        )
+        fast = matrix.col((1.0, 0.0, 0.0))
+        slow = matrix.col((0.0, -1.0, 0.0))
+        cntr = matrix.col((0.0, 0.0, -100.0))
+
+        # shifts to go from the centre to the origin
+        off_x = (self._array_size[0] / 2) * pixel_size[0]
+        shift_x = -1.0 * fast * off_x
+        off_y = (self._array_size[1] / 2) * pixel_size[1]
+        shift_y = -1.0 * slow * off_y
+        orig = cntr + shift_x + shift_y
+
+        d = Detector()
+
+        root = d.hierarchy()
+        root.set_local_frame(fast.elems, slow.elems, orig.elems)
+
+        self.coords = {}
+        panel_idx = 0
+
+        # set panel extent in pixel numbers and x, y mm shifts. Origins taken from
+        # an XDS.INP
+        pnl_data = []
+        pnl_data.append(
+            {"xmin": 0, "ymin": 0, "xmax": 516, "ymax": 516, "xmin_mm": 0, "ymin_mm": 0}
+        )
+
+        # redefine fast, slow for the local frame
+        fast = matrix.col((1.0, 0.0, 0.0))
+        slow = matrix.col((0.0, 1.0, 0.0))
+
+        for ipanel, pd in enumerate(pnl_data):
+            xmin = pd["xmin"]
+            xmax = pd["xmax"]
+            ymin = pd["ymin"]
+            ymax = pd["ymax"]
+            xmin_mm = pd["xmin_mm"]
+            ymin_mm = pd["ymin_mm"]
+
+            origin_panel = fast * xmin_mm + slow * ymin_mm
+
+            panel_name = "Panel%d" % panel_idx
+            panel_idx += 1
+
+            p = d.add_panel()
+            p.set_type("SENSOR_PAD")
+            p.set_name(panel_name)
+            p.set_raw_image_offset((xmin, ymin))
+            p.set_image_size((xmax - xmin, ymax - ymin))
+            p.set_trusted_range(trusted_range)
+            p.set_pixel_size((pixel_size[0], pixel_size[1]))
+            p.set_thickness(thickness)
+            p.set_material("Si")
+            # p.set_mu(mu)
+            # p.set_px_mm_strategy(ParallaxCorrectedPxMmStrategy(mu, t0))
+            p.set_local_frame(fast.elems, slow.elems, origin_panel.elems)
+            p.set_raw_image_offset((xmin, ymin))
+            p.set_gain(3.0)  # exact gain
+            self.coords[panel_name] = (xmin, ymin, xmax, ymax)
+
+        return d
+
+
 class FormatCBFMiniTimepix1032(FormatCBFMiniTimepix):
     @staticmethod
     def understand(image_file):
         """Check to see if this looks like a Timepix mini CBF format image,
         i.e. we can make sense of it."""
-        mime_header = b""
+        mime_header = ""
         in_binary_format_section = False
         for record in FormatCBFMini.open_file(image_file, "rb"):
-            if b"--CIF-BINARY-FORMAT-SECTION--" in record:
+            record = record.decode()
+            if "--CIF-BINARY-FORMAT-SECTION--" in record:
                 in_binary_format_section = True
-            elif in_binary_format_section and record.startswith(b"X"):
+            elif in_binary_format_section and record.startswith("X"):
                 mime_header += record
             if in_binary_format_section and len(record.strip()) == 0:
                 # http://sourceforge.net/apps/trac/cbflib/wiki/ARRAY_DATA%20Category
@@ -284,9 +386,9 @@ class FormatCBFMiniTimepix1032(FormatCBFMiniTimepix):
                 break
 
         # Look for 1032 pixels
-        for record in mime_header.split(b"\n"):
+        for record in mime_header.split("\n"):
             if (
-                b"-Binary-Size-Fastest-Dimension:" in record
+                "-Binary-Size-Fastest-Dimension:" in record
                 and int(record.split()[1]) == 1032
             ):
                 return True
@@ -399,13 +501,3 @@ class FormatCBFMiniTimepix1032(FormatCBFMiniTimepix):
             self.coords[panel_name] = (xmin, ymin, xmax, ymax)
 
         return d
-
-
-if __name__ == "__main__":
-
-    import sys
-
-    for arg in sys.argv[1:]:
-        print(FormatCBFMiniTimepix.understand(arg))
-        print(FormatCBFMiniTimepix512.understand(arg))
-        print(FormatCBFMiniTimepix1032.understand(arg))
