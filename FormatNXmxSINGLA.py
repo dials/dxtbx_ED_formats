af6e2d05bca54b0a885594d2a0f21d74edd48f2e
David Waterman <dgwaterman@gmail.com>
2022-09-09 09:47:13 +0100

    Update FormatNXmxSINGLA for trusted_range changes in https://github.com/cctbx/dxtbx/pull/536

--- FormatNXmxSINGLA.py
+++ FormatNXmxSINGLA.py
@@ -122,7 +122,7 @@ class FormatNexusSINGLA(FormatNXmx):
         pixel_size = self._pixel_size
         image_size = self._image_size
         dyn_range = self._bit_depth_readout
-        trusted_range = (-1, 2**dyn_range - 1)
+        trusted_range = (0, 2**dyn_range - 1)
         beam_centre = [(p * i) / 2 for p, i in zip(pixel_size, image_size)]
         d = self._detector_factory.simple(
             "PAD", 2440, beam_centre, "+x", "-y", pixel_size, image_size, trusted_range
